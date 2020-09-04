90b1d12dc72ddd5486e9520426dd737322546335
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-04 17:40:59 +0530

    Changing style of HTML with Javascript

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ document.getElementsByClassName("list")[2].style.color = "red";
 document.getElementsByTagName("h1")[0].innerHTML = "Good Bye";
 document.getElementById("btn").style.fontSize = "1rem";
 document.querySelector(".heading").style.fontSize = "5rem";
+document.querySelector("button").classList.add("change");
