0834e750fa9236c1b1822f6d557b7a5318c8c5ab
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-04 17:34:40 +0530

    Selecting HTML elements with Javascript

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-document.query
+document.getElementsByClassName("list")[2].style.color = "red";
+document.getElementsByTagName("h1")[0].innerHTML = "Good Bye";
+document.getElementById("btn").style.fontSize = "1rem";
+document.querySelector(".heading").style.fontSize = "5rem";
