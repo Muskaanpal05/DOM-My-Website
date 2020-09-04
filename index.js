739fd52443c7c6773ad4b017396598c5a9192bff
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-04 17:47:05 +0530

    Text manipulation and text content

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Selecting HTML elements with Javascript
 document.getElementsByClassName("list")[2].style.color = "red";
 document.getElementsByTagName("h1")[0].innerHTML = "Good Bye";
 document.getElementById("btn").style.fontSize = "1rem";
 document.querySelector(".heading").style.fontSize = "5rem";
+
+
+// Changing style of HTML with Javascript
 document.querySelector("button").classList.add("change");
+
+document.querySelector("h1").textContent = "Hy there!";
+document.querySelector("h1").textContent = "Hello there!";
